200aede32b524d5c9c8a214b3e5d0c6af6939da4
angelgabriel1020 <67980568+angelgabriel1020@users.noreply.github.com>
2021-01-15 17:29:07 -0600

    Update database.sql
    Se añade la tabla alumno

--- backend/database.sql
+++ backend/database.sql
@@ -14,5 +14,27 @@ create table horarios (
     disponible int not null
 );
 
+-- Creacion de la tabla alumno
+
+create table alumno (
+    boleta char(10) primary key not null,
+    nombre varchar(50) not null,
+    apellidop varchar(50) not null,
+    apellidom varchar(50) not null,
+    daten date not null,
+    sexo bool not null,
+    curp char(18) not null,
+    calle varchar(50) not null,
+    colonia varchar(50) not null,
+    codpostal int(5) not null,
+    tel varchar(10) not null,
+    correo varchar(60) not null,
+    escuela varchar(8) not null,
+    entidad varchar(19) not null,
+    nombreE varchar(50),
+    promedio double(4,2) not null,
+    escom int(1) not null
+);
+
 
 
