6566b4924ff81f7dcf58de2b9e296432207fe980
jcontrerasroberto <contreras.jrobert@gmail.com>
2021-01-16 00:30:00 -0600

    Envio de datos del formulario via ajax listo

--- scripts/confirmation.js
+++ scripts/confirmation.js
@@ -1,32 +1,32 @@
-function llenarInfo(){
+function llenarInfo() {
     const entradas = document.querySelectorAll('#form input');
     const selectores = document.querySelectorAll('#form select');
     entradas.forEach(
         element => {
-            if(element.name != "") {
-                pos = document.getElementById("mostrar_"+element.name);
+            if (element.name != "") {
+                pos = document.getElementById("mostrar_" + element.name);
                 pos.innerHTML = '';
-                if(element.name == "sexo"){
-                    if(element.checked){
+                if (element.name == "sexo") {
+                    if (element.checked) {
                         var node = document.createTextNode((element.value).toUpperCase());
                         pos.appendChild(node);
                     }
-                }else{
+                } else {
                     var node = document.createTextNode((element.value).toUpperCase());
                     pos.appendChild(node);
                 }
-                
+
             }
-            
+
         }
     );
 
     selectores.forEach(
         element => {
             console.log(element.value);
-            pos = document.getElementById("mostrar_"+element.name);
+            pos = document.getElementById("mostrar_" + element.name);
             pos.innerHTML = '';
-            if(element.name == "escuela" && element.value == "otro")
+            if (element.name == "escuela" && element.value == "otro")
                 document.getElementById("label_nombreE").style.display = "block";
             var node = document.createTextNode((element.value).toUpperCase());
             pos.appendChild(node);
@@ -34,6 +34,34 @@ function llenarInfo(){
     )
 }
 
-function guardarDatos(){
-    
+function guardarDatos() {
+    var alumno = {}
+    const entradas = document.querySelectorAll('#form input');
+    const selectores = document.querySelectorAll('#form select');
+    entradas.forEach(
+        element => {
+            if (element.name != "") {
+                alumno[element.name] = element.value;
+            }
+
+        }
+    );
+    selectores.forEach(
+        element => {
+            alumno[element.name] = element.value;
+        }
+    )
+    var jsonformat = JSON.stringify(alumno);
+
+    $.ajax({
+        url: '../backend/conexionbd.php',
+        type: 'post',
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function (data) {
+            console.log(data);
+        },
+        data: jsonformat
+    });
+
 }
