a837407b8183f41f27a6f9e15ceb3ba30e6d9d78
jcontrerasroberto <contreras.jrobert@gmail.com>
2021-01-16 16:13:10 -0600

    Registro del alumno completo

--- backend/insertar_alumno.php
+++ backend/insertar_alumno.php
@@ -1,8 +1,13 @@
 <?php
 
     require_once('openconnectiondb.php');
-    $data = json_decode( file_get_contents( 'php://input' ), true );  
-    $horario = 5;
+    $data = json_decode( file_get_contents( 'php://input' ), true );
+
+    $horario = 0;
+    $lugares_ocupados = 0;
+
+    require_once('seleccion_horario.php'); 
+    
     $sexo = 0;
     $n_opcion = 0;
     
@@ -36,10 +41,37 @@
 
     $result = mysqli_stmt_execute($accion);
 
-    $return_arr[] = array("success" => $result);
+    if (!$result){
+        $return_arr[] = array("success" => $result);
+        echo json_encode($return_arr);
+        exit(1);
+    }
+
+    $lugares_ocupados++;
+
+    $sql_update = "UPDATE horarios SET lugares_ocupados= '$lugares_ocupados' WHERE horario_id= $horario";
+    $resultados_update= $enlace->query($sql_update);
+
+    if (!$resultados_update){
+        $return_arr[] = array("success" => $resultados_update);
+        echo json_encode($return_arr);
+        exit(1);
+    }
+
+    if ($lugares_ocupados==25){
+        $sql_update = "UPDATE horarios SET disponible = 0 WHERE horario_id = $horario";
+        $resultados_update = $enlace->query($sql_update);
+
+        if (!($resultados_update)){
+            $return_arr[] = array("success" => $resultados_update);
+            echo json_encode($return_arr);
+            exit(1);
+        }
+    }
+
+    $return_arr[] = array("success" => $resultados_update);
 
     mysqli_stmt_close($accion);
-
 
     echo json_encode($return_arr);
 
---
+++ backend/seleccion_horario.php
@@ -0,0 +1,28 @@
+<?php
+
+    $sql = "SELECT * FROM horarios WHERE disponible=1 LIMIT 1";
+    $resultados_horarios = $enlace->query($sql);
+
+    if (!($resultados_horarios)){
+        $return_arr[] = array("success" => $resultados_update);
+        echo json_encode($return_arr);
+        exit(1);
+    }
+
+    
+    if (mysqli_num_rows($resultados_horarios)==0) { 
+        echo "No hay horarios disponibles";
+        $return_arr[] = array("success" => $resultados_update);
+        echo json_encode($return_arr);
+        exit(1);
+    }
+  
+    
+    while($row = mysqli_fetch_array($resultados_horarios)) {
+        
+        $horario = $row['horario_id'];
+        $lugares_ocupados = $row['lugares_ocupados'];
+    
+    }
+
+?>
