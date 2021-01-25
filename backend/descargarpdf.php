34c974ac511839389c9f14f0b8174a09a4b0d0ff
José Roberto Contreras Barrita <contreras.jrobert@gmail.com>
2021-01-25 16:45:14 -0600

    Update descargarpdf.php

--- backend/descargarpdf.php
+++ backend/descargarpdf.php
@@ -159,13 +159,13 @@
 	    $mail->isSMTP();                                            // Set mailer to use SMTP
 	    $mail->Host       = 'smtp.gmail.com';  // Specify main and backup SMTP servers
 	    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-	    $mail->Username   = 'cifi.contacto@gmail.com';                     // SMTP username
-	    $mail->Password   = 'recuperemosCIFI';                               // SMTP password
+	    $mail->Username   = '';                     // SMTP username
+	    $mail->Password   = '';                               // SMTP password
 	    $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
 	    $mail->Port       = 587;                                    // TCP port to connect to
 
 	    //Recipients
-	    $mail->setFrom('cifi.contacto@gmail.com', 'ESCOM');
+	    $mail->setFrom('', 'ESCOM');
 	    $mail->addAddress($row['correo']);     // Add a recipient
 
 
